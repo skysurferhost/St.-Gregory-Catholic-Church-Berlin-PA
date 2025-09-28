c2efbb461a06e4a5c9b8827117f8e5d37fe5468b
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2025-09-27 22:40:40 -0400

    360 Update

---
+++ data.js
@@ -0,0 +1,737 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-st-gregory-catholic-church-berlin-pa",
+      "name": "St. Gregory Catholic Church Berlin, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": 0.008693203000946781,
+        "pitch": 0,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.007367036848542341,
+          "pitch": -0.3744770629536376,
+          "rotation": 0,
+          "target": "1-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": 0.32011528797833044,
+          "pitch": 0.1610653191434288,
+          "rotation": 0,
+          "target": "5-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -0.039895543979973525,
+          "pitch": 0.8940364687640834,
+          "rotation": 0,
+          "target": "3-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -0.011388989029853747,
+          "pitch": 0.5406639314044277,
+          "rotation": 0,
+          "target": "4-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -0.011994514928080235,
+          "pitch": -0.06061267208919219,
+          "rotation": 0,
+          "target": "2-st-gregory-catholic-church-berlin-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-st-gregory-catholic-church-berlin-pa",
+      "name": "St. Gregory Catholic Church Berlin, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.007092024732703095,
+          "pitch": 0.22282469162014884,
+          "rotation": 0,
+          "target": "0-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -0.9183436705047594,
+          "pitch": 1.0523348709543505,
+          "rotation": 0,
+          "target": "5-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -0.002280315504410879,
+          "pitch": 0.8058340841963805,
+          "rotation": 0,
+          "target": "2-st-gregory-catholic-church-berlin-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-st-gregory-catholic-church-berlin-pa",
+      "name": "St. Gregory Catholic Church Berlin, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.001845498596960482,
+          "pitch": -0.44237815368040145,
+          "rotation": 0,
+          "target": "1-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": 0.5979747133735831,
+          "pitch": 0.5579054080182608,
+          "rotation": 0,
+          "target": "5-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -3.1316024429057325,
+          "pitch": 0.9427553122277441,
+          "rotation": 0,
+          "target": "3-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -3.121492915132711,
+          "pitch": 1.42743955336553,
+          "rotation": 0,
+          "target": "4-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -3.140877664251615,
+          "pitch": 0.08119319577659212,
+          "rotation": 0,
+          "target": "0-st-gregory-catholic-church-berlin-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-st-gregory-catholic-church-berlin-pa",
+      "name": "St. Gregory Catholic Church Berlin, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.008882083180017375,
+          "pitch": 0.26040852745722987,
+          "rotation": 0,
+          "target": "4-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": 0.0037233746533011924,
+          "pitch": -0.8125990681947854,
+          "rotation": 0,
+          "target": "1-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -3.127646267967812,
+          "pitch": -0.5721192498756089,
+          "rotation": 0,
+          "target": "0-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": 0.00521186642239968,
+          "pitch": -0.47975058597807063,
+          "rotation": 0,
+          "target": "2-st-gregory-catholic-church-berlin-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-st-gregory-catholic-church-berlin-pa",
+      "name": "St. Gregory Catholic Church Berlin, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0840998769268246,
+          "pitch": -0.4464884382741232,
+          "rotation": 0,
+          "target": "0-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -3.077716343044541,
+          "pitch": 0.32085859020678065,
+          "rotation": 0,
+          "target": "3-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -0.011102434396292438,
+          "pitch": 0.0022204377523475927,
+          "rotation": 0,
+          "target": "6-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": 0.005665730293431537,
+          "pitch": -0.791806767504017,
+          "rotation": 0,
+          "target": "2-st-gregory-catholic-church-berlin-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-st-gregory-catholic-church-berlin-pa",
+      "name": "St. Gregory Catholic Church Berlin, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2869834323224154,
+          "pitch": -1.0831410315889691,
+          "rotation": 0,
+          "target": "1-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -2.348301301124362,
+          "pitch": -0.36916079601649265,
+          "rotation": 0,
+          "target": "0-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -1.9771633175787837,
+          "pitch": -0.7115857773187866,
+          "rotation": 0,
+          "target": "2-st-gregory-catholic-church-berlin-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-st-gregory-catholic-church-berlin-pa",
+      "name": "St. Gregory Catholic Church Berlin, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.01332268266264336,
+          "pitch": 0.29529885097135633,
+          "rotation": 0,
+          "target": "7-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": 3.1236343587803352,
+          "pitch": 0.6549371658047232,
+          "rotation": 0,
+          "target": "4-st-gregory-catholic-church-berlin-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-st-gregory-catholic-church-berlin-pa",
+      "name": "St. Gregory Catholic Church Berlin, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.125238302636525,
+          "pitch": 0.39751434998161095,
+          "rotation": 0,
+          "target": "4-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -3.1198622523153627,
+          "pitch": -0.07802400100039542,
+          "rotation": 0,
+          "target": "6-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": 0.012565372399388153,
+          "pitch": 0.3989866532772677,
+          "rotation": 0,
+          "target": "8-st-gregory-catholic-church-berlin-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-st-gregory-catholic-church-berlin-pa",
+      "name": "St. Gregory Catholic Church Berlin, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.127260284067244,
+          "pitch": 0.4232515812919928,
+          "rotation": 0,
+          "target": "7-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": 3.1285239206074857,
+          "pitch": -0.1118442882290136,
+          "rotation": 0,
+          "target": "6-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -0.005814375813365302,
+          "pitch": 0.19297142814880175,
+          "rotation": 0,
+          "target": "9-st-gregory-catholic-church-berlin-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-st-gregory-catholic-church-berlin-pa",
+      "name": "St. Gregory Catholic Church Berlin, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1349051526679066,
+          "pitch": 0.24510503371551806,
+          "rotation": 0,
+          "target": "8-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -3.0572383774416494,
+          "pitch": -1.2492807617257533,
+          "rotation": 0,
+          "target": "10-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -0.0037744829928705315,
+          "pitch": 0.18675216046471732,
+          "rotation": 0,
+          "target": "11-st-gregory-catholic-church-berlin-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-st-gregory-catholic-church-berlin-pa",
+      "name": "St. Gregory Catholic Church Berlin, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1412159040914105,
+          "pitch": 0.7217487157299693,
+          "rotation": 0,
+          "target": "8-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": 0.012117860594557328,
+          "pitch": 1.2692398716481854,
+          "rotation": 0,
+          "target": "9-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -0.009397928011633638,
+          "pitch": 0.4062309584595525,
+          "rotation": 0,
+          "target": "11-st-gregory-catholic-church-berlin-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-st-gregory-catholic-church-berlin-pa",
+      "name": "St. Gregory Catholic Church Berlin, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1361821603932185,
+          "pitch": 0.2929880382048662,
+          "rotation": 0,
+          "target": "9-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -3.1365300654508186,
+          "pitch": -0.3765115213712562,
+          "rotation": 0,
+          "target": "10-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -0.0222021566075572,
+          "pitch": 0.2665654059250304,
+          "rotation": 0,
+          "target": "12-st-gregory-catholic-church-berlin-pa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-st-gregory-catholic-church-berlin-pa",
+      "name": "St. Gregory Catholic Church Berlin, PA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.002534328714920875,
+          "pitch": 0.35877998028894886,
+          "rotation": 0,
+          "target": "11-st-gregory-catholic-church-berlin-pa"
+        },
+        {
+          "yaw": -0.006029980605397611,
+          "pitch": -0.3938785555997324,
+          "rotation": 0,
+          "target": "10-st-gregory-catholic-church-berlin-pa"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
